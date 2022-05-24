c066d5471a703ba9afccd74e6328fb5d4b945700
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-24 14:17:39 -0500

    updated function to export

--- js/factorial.js
+++ js/factorial.js
@@ -1,9 +1,7 @@
-const factorial = function (num) {
+exports.factorial = function (num) {
   let sum = num;
   for (let i = num; i > 1; i--) {
     sum *= i - 1;
   }
   return sum;
 };
-
-console.log(factorial(5));
